51f6ca73196cd3e4fd27b782048c6ec19fb98284
Walber Vaz <wvs.walber@gmail.com>
2024-07-11 17:13:11 -0300

    feat: java doc

--- src/main/java/br/com/wssoftwares/fundamentos/PrimeiroPrograma.java
+++ src/main/java/br/com/wssoftwares/fundamentos/PrimeiroPrograma.java
@@ -1,6 +1,19 @@
 package br.com.wssoftwares.fundamentos;
 
+/**
+ * Primeiro programa em Java
+ *
+ * @author w2k
+ * @since JDK21
+ * @version 1.0
+ * @see System
+ */
 public class PrimeiroPrograma {
+    /**
+     * Método principal
+     *
+     * @param args argumentos passados por linha de comando
+     */
     public static void main(String[] args) {
         System.out.println("Primeiro programa em Java");
     }

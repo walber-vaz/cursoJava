fef5f37269942e62abaf4f157b18f93ae5fba77a
Walber Vaz <wvs.walber@gmail.com>
2024-07-21 21:36:47 -0300

    feat: struct conditional

---
+++ src/main/java/br/com/wssoftwares/fundamentos/EstruturasCondicionais.java
@@ -0,0 +1,13 @@
+package br.com.wssoftwares.fundamentos;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+        // Estrutura condicional if
+        int idade = 18;
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}

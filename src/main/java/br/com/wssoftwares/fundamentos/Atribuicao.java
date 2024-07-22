5dd8ed2bc450001c6b0aa8e753d920409fcaa973
Walber Vaz <wvs.walber@gmail.com>
2024-07-21 21:32:52 -0300

    feat: operation attribution

---
+++ src/main/java/br/com/wssoftwares/fundamentos/Atribuicao.java
@@ -0,0 +1,23 @@
+package br.com.wssoftwares.fundamentos;
+
+public class Atribuicao {
+    public static void main(String[] args) {
+        // Operador de atribuição
+        // = += -= *= /= %=
+        int a = 10;
+        a += 5; // a = a + 5
+        System.out.println(a);
+
+        a -= 5; // a = a - 5
+        System.out.println(a);
+
+        a *= 5; // a = a * 5
+        System.out.println(a);
+
+        a /= 5; // a = a / 5
+        System.out.println(a);
+
+        a %= 5; // a = a % 5
+        System.out.println(a);
+    }
+}
